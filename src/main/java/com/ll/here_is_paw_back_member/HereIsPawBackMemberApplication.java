02de7adfce618e7e1ae4f9011e78650219f25d00
이건학 <kknaks@igeonhag-ui-MacBookAir.local>
2025-03-05 19:33:22 +0900

    Deploy deploy:7

--- src/main/java/com/ll/here_is_paw_back_member/HereIsPawBackMemberApplication.java
+++ src/main/java/com/ll/here_is_paw_back_member/HereIsPawBackMemberApplication.java
@@ -1,13 +1,23 @@
 package com.ll.here_is_paw_back_member;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+@Slf4j
 public class HereIsPawBackMemberApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HereIsPawBackMemberApplication.class, args);
-	}
+		ConfigurableApplicationContext context = SpringApplication.run(HereIsPawBackMemberApplication.class, args);
 
+		// 스프링 컨텍스트에서 값 가져오기
+		String frontUrl = context.getEnvironment().getProperty("custom.site.frontUrl");
+		String backUrl = context.getEnvironment().getProperty("custom.site.backUrl");
+
+		log.info("siteFrontUrl: {}", frontUrl);
+		log.info("siteBackUrl: {}", backUrl);
+	}
 }
